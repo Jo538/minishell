23305f2fe2b74478bbeb139725f12e89ca501bfa
Jo <admin@admins-MacBook-Pro.local>
2026-04-14 15:17:06 +0200

    added cleaner structure to our project. Source files in src, .h in includes. Created a functional, ready to use Makefile. Added main.c that writes our prompt forever unless signal is received, also implemented the history feature to remember what was typed before.

---
+++ libft/ft_putendl_fd.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jchartie <jchartie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/24 12:48:57 by jchartie          #+#    #+#             */
+/*   Updated: 2025/11/24 14:18:43 by jchartie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return ;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+	write(fd, "\n", 1);
+}
+/*
+int main(void)
+{
+	char *s = "";
+	int fd = 1;
+	ft_putendl_fd(s, fd);
+}*/

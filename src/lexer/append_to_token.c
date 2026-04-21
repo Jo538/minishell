8fc590082129fac4019f7091c3d7f43892032ab1
jchartie <jchartie@f6r4s15.paris.42.school>
2026-04-21 14:42:17 +0200

    functions added to handle new segment or change of token type

--- src/lexer/append_to_token.c
+++ src/lexer/append_to_token.c
@@ -6,7 +6,7 @@
 /*   By: jchartie <jchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 19:33:16 by admin             #+#    #+#             */
-/*   Updated: 2026/04/21 13:45:44 by jchartie         ###   ########.fr       */
+/*   Updated: 2026/04/21 14:30:08 by jchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ void	add_new_segment(t_state current_state, t_segment *segment, t_error *err)
 		*err = ERR_MALLOC;
 		return ;
 	}
+	while (segment->next)
+		segment = segment->next;
 	segment->next = new_segment;
 	new_segment->next = NULL;
 	new_segment->quote_type = current_state.quoting;
@@ -47,7 +49,31 @@ void	add_new_segment(t_state current_state, t_segment *segment, t_error *err)
 	segment->value[0] = current_state.c;
 }
 
-void	append_to_segment(t_state current_state, t_segment *segment)
+void	append_to_segment(t_state current_state, t_segment *segment, t_error *err)
 {
+	int		len;
+	char	*new_value;
 	
+	len = ft_strlen(segment->value);
+	new_value = ft_calloc(1, len + 2);
+	if (!new_value)
+	{
+		*err = ERR_MALLOC;
+		return ;
+	}
+	ft_strlcpy(new_value, segment->value, len + 2);
+	new_value[len] = current_state.c;
+	while (segment->next)
+		segment = segment->next;
+	free(segment->value);
+	segment->value = new_value;
+}
+
+void	change_token_type(t_state current_state, t_token *last_token, t_error *err)
+{
+		if (current_state.c == '<')
+			last_token->type = HEREDOC;
+		else
+			last_token->type = APPEND_OUT_DIR;
+		append_to_segment(current_state, last_token->segment, err);
 }
